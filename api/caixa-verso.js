a977ca4cf932e07d30f999f7973d128afa6ab010
Super Nani <103215804+elainesilva13@users.noreply.github.com>
2026-04-25 01:56:41 -0300

    :)

--- api/caixa-verso.js
+++ api/caixa-verso.js
@@ -7,5 +7,5 @@ export default async function handler(req, res) {
         return res.status(405).json({ error: 'Cliente Inelegível' });
     }
     
-    return res.status(200).json({ discount: taxa });
+    return res.status(200).json({ taxaJuros: taxa });
 }
