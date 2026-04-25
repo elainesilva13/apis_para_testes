2902d43f64877833abc4b3455534e1d0d96bf4f4
Super Nani <103215804+elainesilva13@users.noreply.github.com>
2026-04-25 01:20:40 -0300

    :)

--- api/checkClient.js
+++ api/checkClient.js
@@ -1,16 +1,11 @@
 export default async function handler(req, res) {
-    if (req.method === 'GET') {
-        const { idClient } = req.query;
-
-        if (!idClient) {
-            return res.status(400).json({ error: 'Cliente inelegível' });
-        }
-
-        // Generate a random decimal value between 1.00 and 5.99 with 2 decimal places
-        const randomValue = (Math.random() * (5.99 - 1.00) + 1.00).toFixed(2);
-        
-        return res.status(200).json({ discount: parseFloat(randomValue) });
-    } else {
+    if (req.method !== 'GET') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
+    const taxa = (Math.random() * (6.99 - 1.00) + 1.00).toFixed(2);    
+    if (taxa>=6){
+        return res.status(405).json({ error: 'Cliente Inelegível' });
+    }
+    
+    return res.status(200).json({ discount: parseFloat(taxa) });
 }
