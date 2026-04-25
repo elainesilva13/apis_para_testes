8140e7397ee445b62eaa67dc9231f2716b0807bd
Super Nani <103215804+elainesilva13@users.noreply.github.com>
2026-04-25 01:00:28 -0300

    :)

--- api/checkClient.js
+++ api/checkClient.js
@@ -3,11 +3,15 @@ export default async function handler(req, res) {
         const { idClient } = req.query;
 
         if (!idClient) {
-            return res.status(400).json({ error: 'idClient obrigatório' });
+            return res.status(400).json({ error: 'Parâmetro "idClient" obrigatório não informado' });
         }
 
         const discount = (Math.random() * (6.99 - 1) + 1).toFixed(2);
 
+        if(discount >= 6){
+            return res.status(400).json({ error: 'Parâmetro "idClient" obrigatório não informado' });
+        }
+        
         return res.status(200).json({ discount: discount });
     } else {
         return res.status(405).json({ error: 'Method not allowed' });
