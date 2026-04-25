b3951da9d5e0c1279fe03b36f91e4017c6861afe
Super Nani <103215804+elainesilva13@users.noreply.github.com>
2026-04-25 00:55:30 -0300

    :)

--- api/checkClient.js
+++ api/checkClient.js
@@ -1,20 +1,15 @@
 export default async function handler(req, res) {
-    // Check if the request method is GET
     if (req.method === 'GET') {
         const { idClient } = req.query;
 
-        // Validate if idClient is provided
         if (!idClient) {
-            return res.status(400).json({ error: 'idClient parameter is required' });
+            return res.status(400).json({ error: 'idClient obrigatório' });
         }
 
-        // Generate a random discount value between 1 and 5
-        const discount = Math.floor(Math.random() * 5) + 1;
+        const discount = (Math.random() * (6.99 - 1) + 1).toFixed(2);
 
-        // Return the discount value
         return res.status(200).json({ discount: discount });
     } else {
-        // If the method is not GET, return 405 Method Not Allowed
         return res.status(405).json({ error: 'Method not allowed' });
     }
 }
