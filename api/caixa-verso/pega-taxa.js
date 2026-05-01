6a68e89f7e30802eb2b87eff187f8b00f1e4c68f
Super Nani <103215804+elainesilva13@users.noreply.github.com>
2026-05-01 16:27:32 -0300

    Update pega-taxa.js

--- api/caixa-verso/pega-taxa.js
+++ api/caixa-verso/pega-taxa.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     }
     const taxa = parseFloat((Math.random() * (6.99 - 1.00) + 1.00).toFixed(2));    
     if (taxa>=6){
-        return res.status(405).json({ error: 'Cliente Inelegível' });
+        return res.status(401).json({ error: 'Cliente Inelegível' });
     }
     
     return res.status(200).json({ taxaJuros: taxa });
