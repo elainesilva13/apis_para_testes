fcec2d66a0c48befb47814b5c9bdae94590fe9aa
Super Nani <103215804+elainesilva13@users.noreply.github.com>
2026-05-01 19:47:10 -0300

    Desfiz o teste  pega-taxa.js

--- api/caixa-verso/pega-taxa.js
+++ api/caixa-verso/pega-taxa.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
         return res.status(405).json({ error: 'Method not allowed' });
     }
     const taxa = parseFloat((Math.random() * (6.99 - 1.00) + 1.00).toFixed(2));    
-    if (taxa>=1){
+    if (taxa>=6){
         return res.status(400).json({ error: 'Cliente Inelegível' });
     }
     
